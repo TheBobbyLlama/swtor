838cab55317d36f2644a48e41815772a8c2c07ae
TheBobbyLlama <thebobbyllama@yahoo.com>
2026-02-09 00:47:56 -0600

    Experimental: Rich Discord embeds?

--- src/screens/ViewProfile/ViewProfile.js
+++ src/screens/ViewProfile/ViewProfile.js
@@ -6,11 +6,48 @@ import Profile from "../../components/Profile/Profile";
 
 import characterFuncs from "../../db/character";
 import { localize } from "../../localization";
-import { dbTransform, stringKey } from "../../util";
+import { dbTransform, stringKey, stripMarkdown } from "../../util";
 
 import spinner from "../../assets/images/spinner.gif";
 
-// const characterDB = require("../../data/characters.json");
+function createEmbedSummary(metadata, profile) {
+	let result = `**${localize("LABEL_SPECIES")}:** ${localize(metadata.species)}
+**${localize("LABEL_GENDER")}:** ${localize(metadata.gender)}`;
+
+	if (profile.age) {
+		result += `
+**${localize("LABEL_AGE")}:** ${stripMarkdown(profile.age)}`
+	}
+
+	if (metadata.homeworld) {
+		result += `
+**${localize("LABEL_HOMEWORLD")}:** ${stripMarkdown(metadata.homeworld)}`
+	}
+
+	profile.customFields?.forEach(field => {
+		result += `
+**${stripMarkdown(field[0])}:** ${stripMarkdown(field[1])}`
+	});
+
+	return result;
+}
+
+function setEmbedItem(content, property) {
+	const metaElement = document.head.querySelector(`meta[property='${property}']`);
+
+	if (metaElement) {
+		if (content) {
+			metaElement.setAttribute("content", content);
+		} else {
+			metaElement.remove();
+		}
+	} else if (content) {
+		const newElement = document.createElement("meta");
+		newElement.setAttribute("content", content);
+		newElement.setAttribute("property", property);
+		document.head.appendChild(newElement);
+	}
+}
 
 function ViewProfile() {
 	const { characterName, privateKey, server } = useParams();
@@ -34,6 +71,10 @@ function ViewProfile() {
 	if (!characterDB.metadata?.[abbrName] || ((characterDB.metadata[abbrName].private) && (stringKey(characterDB.metadata[abbrName].creator + characterDB.metadata[abbrName].name) !== privateKey))) {
 		return <Null />;
 	}
+
+	setEmbedItem(`${characterDB.metadata[abbrName].name}`, "og:title");
+	setEmbedItem(createEmbedSummary(characterDB.metadata[abbrName], characterDB.profiles[abbrName]), "og:description");
+	setEmbedItem(characterDB.profiles[abbrName].image, "og:image");
 	
 	return <Profile metadata={characterDB.metadata[abbrName]} profileData={characterDB.profiles[abbrName]} />
 }
